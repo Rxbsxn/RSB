8a398d602077c3aa5801042af682dbccd4b47118
Robert Harężlak <harezlak.robert7@gmail.com>
2019-12-02 22:39:16 +0100

    Feat: Controller for User model

--- RetroShop/Controllers/UsersController.cs
+++ RetroShop/Controllers/UsersController.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RetroShop.Models;
+using RetroShop.Services;
+
 namespace RetroShop.Controllers
 {
-  public class UsersController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase
   {
-    
+    private readonly UserService _userService;
+
+    public UsersController(UserService userService)
+    {
+      _userService = userService;
+    }
+
+    public ActionResult<List<User>> Get()
+    {
+      return _userService.Get();
+    }
   }
 }
